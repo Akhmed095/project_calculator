db2a8b90e42e4823da95316aec2bac30b785b742
Ahmed95 <ahmedberiev517@gmail.com>
2025-03-07 19:26:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,12 @@
-// let display = document.querySelector(".display");
-
-// let buttons = Array.from(document.querySelectorAll(".btn"));
-
-// buttons.map((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     switch (e.target.innerText) {
-//       case "AC":
-//         display.innerText = "0";
-//         break;
-//         case "⌫":
-//           display.innerText = display.innerText.slice(0, -1) || "0";
-//         break;
-//       case "=":
-//         try {
-//           display.innerText = eval(display.innerText);
-//         } catch (e) {
-//           display.innerText = "Error!";
-//         }
-//         break;
-//       case "%":
-//         let passedText = display.innerText + "/100";
-//         display.innerText = eval(passedText);
-//         break;
-//       default:
-//         if (display.innerText === "0" && e.target.innerText !== ".") {
-//           display.innerText = e.target.innerText;
-//         } else {
-//           display.innerText += e.target.innerText;
-//         }
-//     }
-//   });
-// });
-
-
-
 let display = document.querySelector(".display");
 
 let buttons = Array.from(document.querySelectorAll(".btn"));
 
 buttons.map((btn) => {
   btn.addEventListener("click", (e) => {
+    let value = e.target.innerText.trim();
+    console.log("Нажата кнопка:", value);
+    
     switch (e.target.innerText) {
       case "AC":
         display.innerText = "0";
@@ -59,7 +26,7 @@ buttons.map((btn) => {
         display.innerText = eval(passedText);
         break;
       default:
-        if (display.innerText === "0" && e.target.innerText !== ",") {
+        if (display.innerText === "0" && e.target.innerText !== ".") {
           display.innerText = e.target.innerText;
         } else {
           display.innerText += e.target.innerText;
